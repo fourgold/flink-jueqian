e85f058f70bc1439b23431d05a1969ef0839042f
fourgold <lijinxinok@163.com>
2021-01-19 15:20:57 +0800

    version-rocksDB fix #1 fix #2 fix #3

---
+++ flink-state/src/main/java/com/ecust/checkpoint/Flink07_State_TestFix.java
@@ -0,0 +1,11 @@
+package com.ecust.checkpoint;
+
+/**
+ * @author JueQian
+ * @create 01-19 15:20
+ */
+public class Flink07_State_TestFix {
+    public static void main(String[] args) {
+        System.out.println("test for github");
+    }
+}
